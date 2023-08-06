cea15be05f1c5b37edaa6f061415f04a4253c7f7
Mahmoud_Hassan_Mahmoud <64662860+MahmoudHassan2505@users.noreply.github.com>
2023-08-06 12:43:09 +0300

    Update BankApplication.java

--- OOP/Task 1/Bank/src/BankApplication.java
+++ OOP/Task 1/Bank/src/BankApplication.java
@@ -2,8 +2,8 @@ public class BankApplication {
 
     public static void main(String[] args) {
 Bank b1 =new Bank();
-b1.addAccount(new savingsAccount(2190210,"Ag",2500,0.3f));
-b1.deposit(0,100);
-b1.displayBalance(0);
+b1.addAccount(new savingsAccount(211902109,"Mahmoud",2500,0.3f));
+b1.deposit(211902109,100);
+b1.displayBalance(211902109);
     }
 }
