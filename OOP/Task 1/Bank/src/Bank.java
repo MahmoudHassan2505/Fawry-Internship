568522b33adec128dfab1d94461815c546137ba6
Mahmoud_Hassan_Mahmoud <64662860+MahmoudHassan2505@users.noreply.github.com>
2023-08-06 12:42:32 +0300

    Update Bank.java

--- OOP/Task 1/Bank/src/Bank.java
+++ OOP/Task 1/Bank/src/Bank.java
@@ -7,23 +7,30 @@ public class Bank {
         accounts=new ArrayList<Account>();
     }
     public void addAccount(Account a) {
-        accounts.add(a);
+        accounts.add(a); 
 
     }
-    public void deposit(int num,float amount){
-     if (num>= accounts.size())
-         throw new RuntimeException("User Not Found");
-     else
-         accounts.get(num).deposite(amount);
+    public void deposit(int accountnubmer,float amount){
+        int index=getindex(accountnubmer);
+
+     if (index> accounts.size()) throw new RuntimeException("User Not Found3"+index);
+     accounts.get(index).deposite(amount);
     }
-    public void withdraw(int num,float amount){
-        if (num>= accounts.size())
-            throw new RuntimeException("User Not Found");
-        else
-            accounts.get(num).withdraw(amount);    }
-    public void displayBalance(int num){
-        if (num>= accounts.size())
-        throw new RuntimeException("User Not Found");
-        else
-            System.out.println(accounts.get(num).getAccountBalance());    }
+    public void withdraw(int accountnubmer,float amount){
+        int num=getindex(accountnubmer);
+        if (num>= accounts.size()) throw new RuntimeException("User Not Found2");
+        accounts.get(num).withdraw(amount);    }
+
+    public void displayBalance(int accountnubmer){
+        int num=getindex(accountnubmer);
+        if (num>= accounts.size()) throw new RuntimeException("User Not Found1");
+        System.out.println(accounts.get(num).getAccountBalance());    }
+        public int getindex(int num){
+            for(int i=0;i<accounts.size();i++){
+                if(accounts.get(i).getAccountNumber()==num) return i;
+            }
+            return 100000;
+
+            
+        }
 }
